a10d1f6a95aa43622a0165dce89b13314b105d75
Marcos García <marcosgarciabalboa@gmail.com>
2025-10-20 16:57:24 +0200

    Añadido ejercicio LSP

---
+++ lsp.py
@@ -0,0 +1,36 @@
+# Clase base
+class Vehiculo:
+    def __init__(self):
+        self.acelerar_secs = 0
+        self.frenar_secs = 0
+
+    def acelerar(self):
+        print(f"El vehículo aceleró en {self.acelerar_secs} segundos")
+
+    def frenar(self):
+        print(f"El vehículo frenó en {self.frenar_secs} segundos")
+
+
+class Bicicleta(Vehiculo):
+    def __init__ (self):
+        self.acelerar_secs = 10
+        self.frenar_secs = 3
+
+
+class Coche(Vehiculo):
+    def __init__ (self):
+        self.acelerar_secs = 5
+        self.frenar_secs = 5     
+
+class Moto(Vehiculo):
+    def __init__ (self):
+        self.acelerar_secs = 3
+        self.frenar_secs = 7    
+
+bici = Bicicleta()
+coche = Coche()
+moto = Moto()
+
+bici.acelerar()
+coche.acelerar()
+moto.acelerar()
